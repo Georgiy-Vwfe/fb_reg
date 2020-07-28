6e9b523f05495b2d3350cab19fe5902436b2a12b
Anton Kovalenko <anton.saha@mail.ru>
2020-07-28 16:57:35 +0300

    Fix bug of starting applications

--- src/main/java/com/sixhands/domain/Project.java
+++ src/main/java/com/sixhands/domain/Project.java
@@ -1,8 +1,7 @@
 package com.sixhands.domain;
 
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
