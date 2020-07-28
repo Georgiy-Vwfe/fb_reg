481cfa1342acde252b2ac70728eb16c736c143ba
Eldar Akavov <desert73eagle@gmail.com>
2020-07-28 19:58:47 +0300

    добавлены Controller на страницы register.html, sign-in.html, index.html

--- src/main/java/com/sixhands/controller/InitialController.java
+++ src/main/java/com/sixhands/controller/InitialController.java
@@ -1,13 +1,25 @@
 package com.sixhands.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class InitialController {
 
-    @GetMapping("/test")
-    public String test(){
-        return "test";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/sign-in")
+    public String signIn(){
+        return "sign-in";
+    }
+
 }
