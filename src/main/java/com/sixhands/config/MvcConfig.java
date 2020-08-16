69b8193da62219311889f5ec051251be6d436190
Eldar Akavov <akavoveldar3005@gmail.com>
2020-08-16 17:29:11 +0300

    fix(configuration):Заменил /login на /register и /sign-in

--- src/main/java/com/sixhands/config/MvcConfig.java
+++ src/main/java/com/sixhands/config/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/sign-in").setViewName("sign-in");
     }
 }
