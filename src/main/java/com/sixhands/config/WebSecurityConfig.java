2f8f910a4ac9b7dccc5dbac59f04f34832a85b2f
ars899 <arsenkadaev@mail.ru>
2020-08-22 23:03:59 +0300

    feat(register):Рег идет в БД.

--- src/main/java/com/sixhands/config/WebSecurityConfig.java
+++ src/main/java/com/sixhands/config/WebSecurityConfig.java
@@ -2,23 +2,31 @@ package com.sixhands.config;
 
 import com.sixhands.domain.User;
 import com.sixhands.repository.UserDetailsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
+import javax.validation.ConstraintValidatorContext;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
     private static final String[] PUBLIC_MATCHERS = {
             "/css/**",
             "/js/**",
@@ -29,7 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             "/register",
             "/forget-password",
             "/admin-profile-project",
-            "/activation/"
+            "/activation/",
+            "/greeting"
     };
 
     @Override
@@ -40,31 +49,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/admin-profile-project").permitAll()
+                //.loginPage("/admin-profile-project").permitAll()
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
     }
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth)
+            throws Exception {
 
-/*    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }*/
-
-    @Bean
-    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
-        return map -> {
-            User newUser = new User();
-            return newUser;
-        };
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT user.email, user.password FROM user WHERE user.email=?")
+                .authoritiesByUsernameQuery("sql...");
     }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
 }
