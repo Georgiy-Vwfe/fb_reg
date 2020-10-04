aafd36ef0c428a436ff5d586da3a1f0a14d17594
Eldar Akavov <akavoveldar3005@gmail.com>
2020-10-04 14:48:14 +0300

    fix(Role.java):Перечень вариантов Role и Industry здесь

--- src/main/java/com/sixhands/domain/Role.java
+++ src/main/java/com/sixhands/domain/Role.java
@@ -1,5 +1,51 @@
 package com.sixhands.domain;
 
 public enum Role {
+    //    Architect;
+//    Business Analyst;
+//    Business Owner;
+//    Designer;
+//    Back-end Developer;
+//    Front-end Developer;
+//    Web Developer;
+//    Lawyer;
+//    Marketing;
+//    Project manager;
+//    Product manager;
+//    System Analyst;
+//    Team Lead ;
+//    Technologist;
+//    Tester;
     USER;
 }
+
+//public enum Industry {
+//    Agricultural;
+//    Automobile;
+//    B2B;
+//    B2C;
+//    Banking;
+//    Building;
+//    Chemical;
+//    Charity;
+//    Computers;
+//    Education;
+//    Electronics;
+//    Finance;
+//    FMCG;
+//    Food supply;
+//    HoReCa;
+//    Industrial equipment;
+//    Insurance;
+//    Investment;
+//    Logistics;
+//    Marketing;
+//    Media;
+//    Metallurgy;
+//    Oil and gas;
+//    Power industry;
+//    PR;
+//    Retail;
+//    Social activities;
+//    Telecommunications;
+//}
