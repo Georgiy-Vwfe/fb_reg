3590af14d19d3f0c518642b64b16c7a95f89a160
Eldar Akavov <akavoveldar3005@gmail.com>
2020-10-09 13:54:47 +0300

    fix(java): Test

--- src/main/java/com/sixhands/domain/Role.java
+++ src/main/java/com/sixhands/domain/Role.java
@@ -3,34 +3,3 @@ package com.sixhands.domain;
 public enum Role {
     USER;
 }
-
-//public enum Industry {
-//    Agricultural;
-//    Automobile;
-//    B2B;
-//    B2C;
-//    Banking;
-//    Building;
-//    Chemical;
-//    Charity;
-//    Computers;
-//    Education;
-//    Electronics;
-//    Finance;
-//    FMCG;
-//    Food supply;
-//    HoReCa;
-//    Industrial equipment;
-//    Insurance;
-//    Investment;
-//    Logistics;
-//    Marketing;
-//    Media;
-//    Metallurgy;
-//    Oil and gas;
-//    Power industry;
-//    PR;
-//    Retail;
-//    Social activities;
-//    Telecommunications;
-//}
